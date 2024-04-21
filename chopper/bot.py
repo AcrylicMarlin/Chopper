19ca01a73c51dd4be5572447b975c3b229075f19
AcrylicMarlin <83969862+AcrylicMarlin@users.noreply.github.com>
2024-04-21 18:14:40 -0400

    Bot now cranks and runs, but is not yet functional. Added a welcome image for the welcome message. ignored files which did not need to go with the repo.

---
+++ chopper/bot.py
@@ -0,0 +1,52 @@
+import os
+import typing
+
+from discord.ext import commands
+import discord
+import asqlite
+from dotenv import load_dotenv
+load_dotenv()
+on_ready_message = """
+------------------------
+- ChopperBot is ready! -
+- Latency: {0}ms       -
+- ID: {1}              -
+- Number of users: {2} -
+------------------------"""
+
+class Chopper(commands.Bot):
+
+    important_channels: typing.Dict[str, discord.TextChannel]
+    def __init__(self, *, 
+        intents:discord.Intents,
+        status:discord.Status,
+        activity:discord.Activity):
+        super().__init__(self, intents=intents, status=status, activity=activity)
+
+
+
+    async def on_ready(self):
+        self.guild = self.get_guild(1225527100870758571)
+        self.important_channels = {
+            "welcome": self.guild.get_channel(1225527101357162587),
+            "goodbye": self.guild.get_channel(1225527101357162590),
+            "levels": self.guild.get_channel(1227241618138988697)
+        }
+        latency = round(self.latency * 1000)
+        ID = self.user.id
+        users = len(self.users)
+        print(on_ready_message.format(latency, ID, users))
+
+
+
+    # async def setup_hook(self):
+        
+    async def run(self):
+
+        try:
+            async with self:
+                await self.start(os.getenv("TOKEN"))
+
+        except KeyboardInterrupt:
+            await self.close()
+    
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import asyncio
+
+import discord
+
+from chopper import Chopper
 
 
+intents=discord.Intents.all()
+status=discord.Status.online
+activity = discord.Activity(name = 'One Piece!', type = discord.ActivityType.streaming, url = 'https://www.youtube.com/watch?v=o-YBDTqX_ZU')
 
-def main():
-    print("Hello World!")
-    print("This is a test message.")
-    print("Testing if scheduled updates work")
+
+client = Chopper(intents=intents, status=status, activity=activity)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(client.run())
